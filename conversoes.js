1b9120b30f67a89fc47b742d98dfca23698976b4
diogopolesso <diogopolesso@gmail.com>
2022-10-24 18:54:26 -0300

    var, let, const e conversoes

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// conversão implícita
+
+//const numero = 456;
+//const numeroString = "456";
+
+//console.log(numero == numeroString);
+
+
+// conversão explícita
+
+const numero = 456;
+const numeroString = Number ("456");
+
+console.log(numero + numeroString);
