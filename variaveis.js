68c24a832a390093b1aed00cf7b21bd68d605131
diogopolesso <diogopolesso@gmail.com>
2022-10-24 19:05:07 -0300

    exercicio 2

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+{
+    var respostaDeTudo = 3.14;
+    let idade = 42;
+    const pi = 29;
+
+    console.log(respostaDeTudo, idade, pi);
+
+}
+
+console.log(respostaDeTudo, idade, pi);
