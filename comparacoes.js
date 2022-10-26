01980b091215f29129688c5c053bcb3e4cecee7a
diogopolesso <diogopolesso@gmail.com>
2022-10-26 00:53:15 -0300

    comparações

---
+++ comparacoes.js
@@ -0,0 +1,19 @@
+// == (Comparação Implicita) -> faz a comparação e faz a conversão.
+
+//const numero = 5;
+//const texto = "5";
+
+//console.log (numero == texto); true
+
+// (Comparação Explícita) sempre compara os valores sem fazer a comparação antes.
+// (Boas Praticas).
+
+const numero = 5;
+const texto = "5";
+
+console.log (numero === texto); false
+
+//typeof
+
+console.log(typeof numero);
+console.log(typeof texto);
