29abb1a4a16fd2aadada6435fd1ccdeb8a8aaa0a
diogopolesso <diogopolesso@gmail.com>
2022-10-26 01:18:28 -0300

    ternario

---
+++ ternario.js
@@ -0,0 +1,14 @@
+// Operador if Classico:
+
+const idadeMinima = 18;
+const idadeCliente = 20;
+
+// if (idadeCliente >= idadeMinima){
+//     console.log("cerveja");
+// }   else {
+//     console.log("suco")
+// }
+
+// Operador Ternario:
+
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco");
