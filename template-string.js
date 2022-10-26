bad7c4447ca9e617039c3c6df507d4ba3b41008b
diogopolesso <diogopolesso@gmail.com>
2022-10-26 01:38:08 -0300

    template-string

---
+++ template-string.js
@@ -0,0 +1,16 @@
+// Template Padrão
+
+const nome = "Diogo";
+const idade = 2022 - 1985;
+const cidadeDeNascimento = "Porto Alegre";
+
+// const apresentação = "Meu Nome é " + nome + ", Minha Idade é "
+// + idade + ", e  nasci na Cidade de " + cidadeDeNascimento;
+
+
+// Template tipo String
+
+const apresentação = `Meu Nome é ${nome}, Minha Idade é ${idade}, 
+e nasci na Cidade de ${cidadeDeNascimento}.`;
+
+console.log(apresentação);
